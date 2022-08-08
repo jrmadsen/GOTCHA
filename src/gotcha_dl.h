72fb36fb4ec18cddbc0db2bc707f5b2a3ddff841
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2022-08-08 13:23:30 -0500

    gotcha_init_ext
    - default to not wrapping dlopen and dlsym
    - support calling gotcha_init_ext(1) to enable wrapping dlopen and dlsym

--- src/gotcha_dl.h
+++ src/gotcha_dl.h
@@ -4,7 +4,7 @@
 #include "hash.h"
 #include "tool.h"
 
-void handle_libdl();
+void handle_libdl(int _v);
 extern void update_all_library_gots(hash_table_t *bindings);
 extern long lookup_exported_symbol(const char *name, const struct link_map *lib,
                                    void **symbol);
